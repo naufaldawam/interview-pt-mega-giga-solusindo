40db608cdce76340bc8f4aed1f6de770ac6a1b9c
Naufal <60767590+naufaldawam@users.noreply.github.com>
2023-05-19 15:36:21 +0700

    Update Header.vue

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -22,17 +22,8 @@
         <li class="nav-item active">
           <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
         </li>
-<!--         <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li> -->
-        <li>
-          <router-link to="/">logout</router-link>
+          <router-link to="/" class="nav-link">Logout</router-link>
         </li>
       </ul>
     </div>
