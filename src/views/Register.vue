e44df5999d1831d5c7b0aa80d9b01bb1fcb5a5e5
Naufal <60767590+naufaldawam@users.noreply.github.com>
2023-05-19 16:18:24 +0700

    Update Register.vue dengan menambahkan popup berhasil membuat akun

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,7 +5,7 @@
       <div class="form-group">
         <label for="username">Username:</label>
         <input type="text" id="username" v-model="registerData.username" required>
-        <div v-if="registerData.username.length > 50" class="error">Password melebihi 50 karakter.</div>
+        <div v-if="registerData.username.length > 50" class="error">Username melebihi 50 karakter.</div>
       </div>
       <div class="form-group">
         <label for="profileName">Profile Name:</label>
@@ -15,31 +15,57 @@
       <div class="form-group">
         <label for="password">Password:</label>
         <input type="password" id="password" v-model="registerData.password" required>
+        <div v-if="!isPasswordValid" class="error">Password harus berisi teks dan kalimat.</div>
         <div v-if="registerData.password.length > 50" class="error">Password melebihi 50 karakter.</div>
       </div>
       <button type="submit">Daftar</button>
     </form>
     <router-link to="/login">Sudah Punya Akun</router-link>
 
+    <!-- Popup untuk pemberitahuan akun berhasil dibuat -->
+    <div v-if="isAccountCreated" class="popup">
+      <div class="popup-content">
+        <p>Akun berhasil dibuat!</p>
+        <p>Silakan login menggunakan akun yang baru dibuat.</p>
+        <button @click="closePopup">Tutup</button>
+      </div>
+    </div>
+
+    <!-- Popup untuk pemberitahuan username sudah digunakan -->
+    <div v-if="isUsernameDuplicate" class="popup">
+      <div class="popup-content">
+        <p>Username sudah digunakan!</p>
+        <p>Silakan pilih username lain.</p>
+        <button @click="closePopup">Tutup</button>
+      </div>
+    </div>
   </div>
 </template>
 
-
 <script>
-
 export default {
   data() {
     return {
-      registerData :{
+      registerData: {
         username: '',
         profileName: '',
         password: ''
-      }
+      },
+      isPasswordValid: true,
+      isAccountCreated: false,
+      isUsernameDuplicate: false
     };
   },
   methods: {
-      register() {
-      // Kirim data register menggunakan metode POST
+    register() {
+      // Validasi password (harus berisi teks dan kalimat)
+      const passwordRegex = /[a-zA-Z]+/;
+      this.isPasswordValid = passwordRegex.test(this.registerData.password);
+
+      if (!this.isPasswordValid) {
+        return;
+      }
+
       fetch('http://159.223.57.121:8090/auth/register', {
         method: 'POST',
         headers: {
@@ -49,18 +75,25 @@ export default {
       })
         .then(response => response.json())
         .then(data => {
-          // Lakukan sesuatu dengan data yang diterima setelah register berhasil
-          console.log(data);
-          if(data.status === "OK"){
-            this.$router.push({path: "/", replace : true})
-            this.$router.replace({path: "/"})
+          console.log(data)
+
+          if (data.message === 'REGISTER SUCCESSFUL') {
+            this.isAccountCreated = true;
+            setTimeout(() => {
+              this.$router.push('/login');
+            }, 5000);
+          } else if (data.message === 'USERNAME IS REGISTERED') {
+            this.isUsernameDuplicate = true;
           }
 
         })
         .catch(error => {
-          // Tangani kesalahan jika terjadi
           console.error(error);
         });
+    },
+    closePopup() {
+      this.isAccountCreated = false;
+      this.isUsernameDuplicate = false;
     }
   }
 };
@@ -108,17 +141,35 @@ button:hover {
   background-color: #45a049;
 }
 
-.buttonRegister {
-  padding: 10px 20px;
-  background-color: yellow;
-  color: blue;
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.popup-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 4px;
+  text-align: center;
+}
+
+.popup button {
+  padding: 5px 10px;
+  background-color: #4CAF50;
+  color: white;
   border: none;
   border-radius: 4px;
   cursor: pointer;
 }
 
-.buttonRegister:hover{
-  background-color: blue;
+.popup button:hover {
+  background-color: #45a049;
 }
 </style>
-
